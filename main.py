f2b9ad8ad3d28c06da612e814c0c113602c796b9
Danthuluri sai hemanth varma <119394989+danthulurisaihemanth@users.noreply.github.com>
2025-03-29 11:34:01 +0530

    main.py

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 18 13:07:51 2020
+Created on Tue Nov 17 21:40:41 2020
 
 @author: win10
 """
-#pip install fastapi uvicorn
 
 # 1. Library imports
-import uvicorn ##ASGI
+import uvicorn
 from fastapi import FastAPI
+from BankNotes import BankNote
+import numpy as np
+import pickle
+import pandas as pd
 
 # 2. Create the app object
 app = FastAPI()
+pickle_in = open("classifier.pkl", "rb")
+classifier = pickle.load(pickle_in)
 
 # 3. Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
 def index():
-    return {'message': 'Hello, World'}
+    return {'message': 'Welcome to My Bank Note Authentication API'}
 
 # 4. Route with a single parameter, returns the parameter within a message
 #    Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/Welcome')
+@app.get('/{name}')
 def get_name(name: str):
-    return {'Welcome To Krish Youtube Channel': f'{name}'}
+    return {'message': f'Hello {name}, welcome to my FastAPI service!'}
 
+# 5. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted Bank Note with the confidence
+@app.post('/predict')
+def predict_banknote(data: BankNote):
+    data = data.dict()
+    variance = data['variance']
+    skewness = data['skewness']
+    curtosis = data['curtosis']
+    entropy = data['entropy']
+    
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
+    result = "Fake Note" if prediction[0] > 0.5 else "Genuine Bank Note"
+    
+    return {'Prediction': result}
 
-
-# 5. Run the API with uvicorn
+# 6. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-#uvicorn main:app --reload
-
-
-
+    
+# To run: uvicorn app:app --reload
